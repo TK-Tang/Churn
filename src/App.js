389d66f0adf79adeca619d7c727d87dc564841ab
TKTang <tk.gq.tang@gmail.com>
2022-03-22 05:04:30 +1100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { React, Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './components/landing';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function User(props) {
+    return <h1>Hello 123123!</h1>;
+}
+
+function Xyz(props) {
+    return <h1>Hello 123asdfasdf123!</h1>;
+}
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/asdf">
+                        <Landing/>
+                    </Route>
+                    <Route path="/zxcv">
+                        <Xyz/>
+                    </Route>
+                </Switch>
+                asfasdfasdf123
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
